6379f541d88343997b93fa19d928a7fc7535aa8c
Flonja <20887403+Flonja@users.noreply.github.com>
2025-07-08 17:00:05 +0200

    Add 1 more example and update gophertunnel/dragonfly

--- cmd/cmds.go
+++ cmd/cmds.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ThronesMC/camera"
 	"github.com/ThronesMC/camera/instructions"
 	"github.com/df-mc/dragonfly/server/cmd"
+	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/world"
 	"github.com/go-gl/mathgl/mgl32"
@@ -25,6 +26,12 @@ func init() {
 		}),
 		Position: protocol.Option(mgl32.Vec3{0, 30, 0}),
 		Facing:   protocol.Option(mgl32.Vec3{0, 0, 0}),
+	})))
+	cmd.Register(cmd.New("fades", "raahhh im cool", nil, NewCameraCommand(instructions.FadeCameraInstruction{
+		FadeInDuration:  500 * time.Millisecond,
+		WaitDuration:    1 * time.Second,
+		FadeOutDuration: 250 * time.Millisecond,
+		Colour:          item.ColourYellow().RGBA(),
 	})))
 	cmd.Register(cmd.New("clear", "back to boring", nil, NewCameraCommand(instructions.ClearCameraInstruction{})))
 }
